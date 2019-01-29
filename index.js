5b972bd761348ed92b9f2d637e0d4e73093af360
sabinasabine <46841900+sabinasabine@users.noreply.github.com>
2019-01-29 21:59:45 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<div class="wrapper1">
+    <div class="parent">
+        <div>
+            <div>
+                <div>
+                    <div>
+                        <div>
+                            <div>
+                                <div class="deepChild"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="blockName"></div>
+</div>
+
+<div class="wrapper2">
+    <div>
+        <div>
+            <div class="sibling first">
+                <div>
+                    <div>
+                        <div>
+                            <div class="childChild"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="sibling second">
+                <div>
+                    <div>
+                        <div>
+                            <div class="childChild"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+<script src="main.js"></script>
+</body>
+</html>
